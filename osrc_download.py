74037237a1c408dead0c73652f7e000389314926
Hendra Manudinata <saya@hendra-manudinata.my.id>
2022-07-09 17:03:01 +0700

    Do not force tqdm to use MB
    Use "B", so tqdm will use "kB", "mB", etc.

--- osrc_download.py
+++ osrc_download.py
@@ -85,7 +85,7 @@ sourceSize = int(requestDown.headers["Content-Length"])
 
 try:
     print(f"\nDownloading {sourceFileName} ({sourceVersion}), please do not terminate the script!")
-    progressBar = tqdm(total=sourceSize, unit="MB", unit_scale=True)
+    progressBar = tqdm(total=sourceSize, unit="B", unit_scale=True)
     with open(sourceFileName, "wb") as file:
         for chunk in requestDown.iter_content(chunk_size=1024 * 1024):
             file.write(chunk)
